ada2e25e6e54d438fed500b04851dae04ab70753
AslamThachapalli <aslam.develop912@gmail.com>
2024-04-24 20:03:36 +0300

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Aslam Thachapalli on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
